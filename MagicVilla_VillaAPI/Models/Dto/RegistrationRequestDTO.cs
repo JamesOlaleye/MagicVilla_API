6c4dd7513b0d38bb3cbbc66ebf27f1f3422c7f01
James Olanipekun <jamesfullstackdeveloper@gmail.com>
2026-03-04 10:22:10 +0100

    Login And Registeration with Identity Implemented

--- MagicVilla_VillaAPI/Models/Dto/RegistrationRequestDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/RegistrationRequestDTO.cs
@@ -8,7 +8,7 @@
 
         public string Password { get; set; }
 
-        public string Role { get; set; }
+        //public string Role { get; set; }
 
         //public string? Email { get; set; }
     }
--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -14,15 +14,17 @@ namespace MagicVilla_VillaAPI.Repository
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private string secretKey;
         private readonly IMapper _mapper;
 
-        public UserRepository(ApplicationDbContext db, IConfiguration configuration, UserManager<ApplicationUser> userManager, IMapper mapper)
+        public UserRepository(ApplicationDbContext db, IConfiguration configuration, UserManager<ApplicationUser> userManager, IMapper mapper, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
             _mapper = mapper;
             _userManager = userManager;
             secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _roleManager = roleManager;
         }
 
         public bool IsUniqueUser(string username)
@@ -59,7 +61,7 @@ namespace MagicVilla_VillaAPI.Repository
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName.ToString()),
                     new Claim(ClaimTypes.Role, roles.FirstOrDefault())
                 }), 
                 Expires = DateTime.UtcNow.AddDays(7),
@@ -71,7 +73,6 @@ namespace MagicVilla_VillaAPI.Repository
             {
                 Token = tokenHandler.WriteToken(token),
                 User = _mapper.Map<UserDTO>(user),
-                Role = roles.FirstOrDefault(),
             };
             return loginResponseDTO;
         }
@@ -91,6 +92,10 @@ namespace MagicVilla_VillaAPI.Repository
                 var result = await _userManager.CreateAsync(user, registrationRequestDTO.Password);
                 if(result.Succeeded)
                 {
+                    if(!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult()) {
+                        await _roleManager.CreateAsync(new IdentityRole("Admin")); 
+                        await _roleManager.CreateAsync(new IdentityRole("Customer")); 
+                    }
                     await _userManager.AddToRoleAsync(user, "Admin");
                     var userToReturn = _db.ApplicationUsers.FirstOrDefault(u => u.UserName == registrationRequestDTO.UserName);
                     return _mapper.Map<UserDTO>(userToReturn);
